eae2bf1e536db57b18c008e7317e67b6f3087333
WLPhoenix <w.cory.hughes@gmail.com>
2013-10-18 22:35:59 -0500

    Added more features to compile.sh

--- compile.sh
+++ compile.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-gcc -std=c99 -pendantic-errors -o ./bin/my ./src/my.c
+CONTEXT="`dirname $0`"
+rm -r $CONTEXT/bin 2>/dev/null && mkdir $CONTEXT/bin
+gcc -std=c99 -pendantic-errors -o $CONTEXT/bin/my $CONTEXT/src/my.c
