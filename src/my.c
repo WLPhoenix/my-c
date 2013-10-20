5ae494a78a9cb58510c38bfb57132b64111c619a
Adam R. Wilczek <arwilczek90@gmail.com>
2013-10-20 01:54:33 -0500

    Working create command and execute command

--- src/my.c
+++ src/my.c
@@ -11,6 +11,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <sys/types.h>
 
 #include <sys/stat.h>
 #include <errno.h>
@@ -282,8 +283,145 @@ list_commands()
 { }
 
 int 
-create_command(char* name) 
-{ }
+create_command(const char* name, const char* group, const char* content) 
+{
+  const char* myhome = getenv("MY_HOME");
+  int homelen = strlen(myhome);
+  int namelen = strlen(name);
+  int grouplen = strlen(group);
+
+  char filename[ namelen + 2];
+  memset( filename, 0, (namelen + 2) * sizeof(char) );
+  strcat( filename, "C.");
+  strcat( filename, name);
+
+  //printf( "filename: %s\n", filename );
+
+  //char grouppath[ grouplen + namelen + 5];
+
+  char fullgrouppath[ grouplen + homelen + 2];
+  memset( fullgrouppath, 0, (grouplen + homelen + 2) * sizeof(char) );
+  strcpy( fullgrouppath, myhome );
+  strcat( fullgrouppath, "/");
+  strcat( fullgrouppath, group);
+
+  char fullpath[ homelen + grouplen + namelen + 5 ];
+  memset( fullpath, 0, (homelen + grouplen + namelen + 5) * sizeof(char) );
+  strcpy( fullpath, fullgrouppath);
+  strcat( fullpath, "/");
+  strcat( fullpath, filename);
+
+  char listpath [ homelen + grouplen + 7 ];
+  memset( listpath, 0, (homelen + grouplen + 6) * sizeof(char) );
+  strcpy( listpath, fullgrouppath);
+  strcat( listpath, "/.list");
+  
+
+  //printf( "fullgrouppath: %s\n", fullgrouppath );
+  //printf( "fullpath: %s\n", fullpath );
+  //printf( "listpath: %s\n", listpath );
+  
+  FILE *test = fopen(fullpath,"r");
+
+  struct stat sb;
+  if( stat(fullpath, &sb) == 0)
+  {
+    //printf("File name: %s\n", fullpath);
+    //printf("File status: %i\n", S_ISREG(sb.st_mode) );
+    puts("File already exists");
+    fclose(test);
+    return -1;
+  }
+  else 
+  {
+    FILE *list = fopen(listpath, "a");
+    FILE *fp = fopen(fullpath, "a" );
+    if (NULL != fp) 
+    {
+      fputs( content, fp );
+      fputs( "\n", fp );
+      fflush( fp );
+      fclose( fp );
+
+      if( list != NULL )
+      {
+	fputs( filename, list);
+	fputs( "\n", list );
+	fflush( list);
+	fclose( list);
+      }
+      else 
+      {
+	return -1;
+      }
+    }
+    else 
+    {
+      return -1;
+    }
+  }
+}
+
+int
+execute_command(const char * group, const char * name)
+{
+    const char* myhome = getenv("MY_HOME");
+  int homelen = strlen(myhome);
+  int namelen = strlen(name);
+  int grouplen = strlen(group);
+
+  char filename[ namelen + 2];
+  memset( filename, 0, (namelen + 2) * sizeof(char) );
+  strcat( filename, "C.");
+  strcat( filename, name);
+
+  //printf( "filename: %s\n", filename );
+
+  //char grouppath[ grouplen + namelen + 5];
+
+  char fullgrouppath[ grouplen + homelen + 2];
+  memset( fullgrouppath, 0, (grouplen + homelen + 2) * sizeof(char) );
+  strcpy( fullgrouppath, myhome );
+  strcat( fullgrouppath, "/");
+  strcat( fullgrouppath, group);
+
+  char fullpath[ homelen + grouplen + namelen + 5 ];
+  memset( fullpath, 0, (homelen + grouplen + namelen + 5) * sizeof(char) );
+  strcpy( fullpath, fullgrouppath);
+  strcat( fullpath, "/");
+  strcat( fullpath, filename);
+  
+  FILE * fp = fopen(fullpath,"r");
+  char  buff[1024];
+  fread(buff, sizeof(char),1023, fp);
+  FILE * out;
+  out = popen(buff,"r");
+  if( out == NULL)
+  {
+     puts("Command failed to fork.");
+     return -1;
+  }
+  else
+  { 
+     char output[1053];
+     strcpy(output,"Command: ");
+     strcat(output, buff);
+     strcat(output, "started successfully");
+     puts(output);
+     int c;
+     if(out)
+     {
+          while(( c = getc(out)) != EOF)
+          {
+             putchar(c);
+          }  
+     } 
+     return 0;
+  }
+  
+  
+  
+}
 
 int
 echo_command(char* name)
@@ -305,7 +443,7 @@ list_templates()
 { }
 
 int 
-create_template(char* name) 
+create_template(const char* name, const char * group) 
 { }
 
 int
@@ -350,9 +488,20 @@ int
 main(int argc, char *argv[])
 {
   setup_home();
-  list_groups();
-  create_group("test");
-  create_note("test","adam","semen");
+  DIR *dp;
+  struct dirent *ep;     
+  dp = opendir ("./");
+
+  if (dp != NULL)
+  {
+    while (ep = readdir (dp))
+      if(ep->d_name != ".." || ep -> d_name != ".")
+      puts (ep->d_name);
+
+    (void) closedir (dp);
+  }
+  else
+    perror ("Couldn't open the directory");
   return 0;
 }
 
